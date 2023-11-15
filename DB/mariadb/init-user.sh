55bf83b08ecc93c505ef819b031a7579b108c7c5
johnchen <gcobc27746@gmail.com>
2023-11-15 16:11:29 +0800

    first commit

---
+++ DB/mariadb/init-user.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/bin/mysqladmin -u root password '12345678' && \
+/usr/bin/mysql -u root -p12345678 -e "GRANT ALL ON *.* TO test@$(%) identified by 'cils92810';" && \
+/usr/bin/mysql -u root -p12345678 -e "FLUSH PRIVILEGES;"
