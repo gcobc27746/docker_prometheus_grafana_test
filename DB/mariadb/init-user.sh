7b5cbbcbdd533e000c133d06e6a6f2bb67630478
johnchen <gcobc27746@gmail.com>
2023-11-15 17:35:22 +0800

    prometheus.yml permission

--- DB/mariadb/init-user.sh
+++ DB/mariadb/init-user.sh
@@ -2,9 +2,7 @@
 
 /usr/bin/mysqladmin -u root password '12345678' && \
 /usr/bin/mysql -u root -p12345678 -e "GRANT ALL ON *.* TO 'test'@'%' identified by '1qaz2wsx';" && \
-
 /usr/bin/mysql -u root -p12345678 -e "CREATE USER 'exporter'@'%' IDENTIFIED BY '123456';" && \
 /usr/bin/mysql -u root -p12345678 -e "GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';" && \
 /usr/bin/mysql -u root -p12345678 -e "GRANT SELECT ON performance_schema.* TO 'exporter'@'%';" && \
-
 /usr/bin/mysql -u root -p12345678 -e "FLUSH PRIVILEGES;"
